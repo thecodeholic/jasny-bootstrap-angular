6739f9bb952e8dc35195c82c73074f588d4c2b95
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2016-12-19 12:04:07 +0400

    Fixed bug: Remove file when you assign file variable to null. Implemented event handler binding

--- DemoController.js
+++ DemoController.js
@@ -1,16 +1,32 @@
 /**
  * Created by Zura on 4/8/2016.
  */
-(function(){
-    angular
-        .module('jasny-bootstrap-angular')
-        .controller('DemoController', ['$scope', DemoController]);
+(function () {
+  angular
+    .module('jasny-bootstrap-angular')
+    .controller('DemoController', ['$scope', DemoController]);
 
-    function DemoController($scope){
-        console.log($scope);
-        $scope.showInput = function(){
-            console.log($scope.file);
-            console.log($('input[type=file]')[0].files);
-        }
+  function DemoController($scope) {
+
+    $scope.showInput = function () {
+      console.log($scope.file);
+      console.log($('input[type=file]')[0].files);
+    };
+
+    $scope.removeFile = function () {
+      $scope.file = null;
+    };
+
+    $scope.inputCleared = function () {
+      console.log("DemoController:inputCleared");
+    };
+
+    $scope.inputChanged = function () {
+      console.log("DemoController:inputChanged");
+    };
+
+    $scope.inputReset = function () {
+      console.log("DemoController:inputReset");
     }
+  }
 })();
--- dist/jasny-bootstrap-angular.js
+++ dist/jasny-bootstrap-angular.js
@@ -7,60 +7,75 @@
 /**
  * Created by Zura on 4/8/2016.
  */
-(function(){
-    angular
-        .module('jasny-bootstrap-angular')
-        .directive('jsFileInput', ['$parse', function($parse){
-            return {
-                restrict: 'AE',
-                replace: true,
-                template: [
-                    '<div class="fileinput fileinput-new" data-provides="fileinput">',
-                        '<div class="fileinput-preview thumbnail" data-trigger="fileinput"></div>',
-                        '<div>',
-                            '<span class="btn btn-default btn-file">',
-                                '<span class="fileinput-new">Select image</span>',
-                                '<span class="fileinput-exists">Change</span>',
-                                '<input type="file" name="ddd">',
-                            '</span>',
-                            '<a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>',
-                        '</div>',
-                    '</div>'
-                ].join(""),
-                link: function (scope, elem, attrs, ngModelCtrl) {
-                    var inputAttrs = JSON.parse(attrs.inputAttrs),
-                        previewAttrs = JSON.parse(attrs.previewAttrs),
-                        $preview = elem.find('.fileinput-preview'),
-                        $input = elem.find('input[type=file]'),
-                        model = $parse(attrs.ngModel),
-                        modelSetter = model.assign
-                     ;
-                    for (var i in inputAttrs){
-                        $input.attr(i, inputAttrs[i]);
-                    }
-                    for (var i in previewAttrs){
-                        $preview.attr(i, previewAttrs[i]);
-                    }
+(function () {
+  angular
+    .module('jasny-bootstrap-angular')
+    .directive('jsFileInput', ['$parse', function ($parse) {
+      return {
+        restrict: 'AE',
+        replace: true,
+        scope: {
+          'file': '=ngModel',
+          'instance': '=',
+          'inputAttrs': '=',
+          'previewAttrs': '=',
+          'fileInputChange': '&',
+          'fileInputClear': '&',
+          'fileInputReset': '&'
+        },
+        template: [
+          '<div class="fileinput fileinput-new" data-provides="fileinput">',
+          '<div class="fileinput-preview thumbnail" data-trigger="fileinput"></div>',
+          '<div>',
+          '<span class="btn btn-default btn-file">',
+          '<span class="fileinput-new">Select image</span>',
+          '<span class="fileinput-exists">Change</span>',
+          '<input type="file">',
+          '</span>',
+          '<a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>',
+          '</div>',
+          '</div>'
+        ].join(""),
+        link: function (scope, $element, attrs, ngModelCtrl) {
+          var inputAttrs = JSON.parse(attrs.inputAttrs),
+            previewAttrs = JSON.parse(attrs.previewAttrs),
+            $preview = $element.find('.fileinput-preview'),
+            $input = $element.find('input[type=file]'),
+            model = $parse(attrs.ngModel),
+            modelSetter = model.assign
+            ;
+          for (var i in inputAttrs) {
+            $input.attr(i, inputAttrs[i]);
+          }
+          for (var i in previewAttrs) {
+            $preview.attr(i, previewAttrs[i]);
+          }
 
-                    $input.bind('change', function(){
-                        var me = this;
-                        scope.$apply(function () {
-                            if (me.files.length > 0) {
-                                modelSetter(scope, me.files[0]);
-                            } else {
-                                modelSetter(scope, null);
-                            }
-                        });
-                    });
+          $input.bind('change', function () {
+            var me = this;
+            scope.$apply(function () {
+              if (me.files.length > 0) {
+                modelSetter(scope, me.files[0]);
+              } else {
+                modelSetter(scope, null);
+              }
+            });
+          });
 
-                    scope.$watch('file', function(value){
-                        if (!value){
-                            $input.val(null);
-                        }
-                    });
+          scope.$watch('file', function (value) {
+            if (!value && $input.val()) {
+              $element.fileinput('clear');
+              $element.trigger('clear.bs.fileinput')
+            }
+          });
 
-                    elem.fileinput();
-                }
-            };
-        }]);
+          $element
+            .on('change.bs.fileinput', scope.fileInputChange)
+            .on('clear.bs.fileinput', scope.fileInputClear)
+            .on('reset.bs.fileinput', scope.fileInputReset)
+            .fileinput()
+          ;
+        }
+      };
+    }]);
 })();
